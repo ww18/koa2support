7513244df03a56fea1c40a9503c576fc0150441f
liwenwen <liwenwen@oak-inc.com>
2018-05-03 17:51:37 +0800

    改为x-tag的1

--- public/js/xPraise.js
+++ public/js/xPraise.js
@@ -3,20 +3,21 @@
  */
 import XTagPraise from './Thumb.js';
 //import css from '../css/index.css';
-xtag.create('x-praise', class CC extends XTagPraise{
-    connectedCallback () {
-        this.start();
-    }
-    start (){
-        this.appendChild(this.container);
-    }
-    stop (){
-
-    }
-    update (){
-
-    }
-    'click::event' (){
-        this.addAnimation();
+xtag.register('x-praise', {
+    prototype: XTagPraise.prototype,
+    lifecycle: {
+        created: function(){
+            this.start();
+        }
+    },
+    methods: {
+        start: function(){
+            this.appendChild(this.container);
+        }
+    },
+    events: {
+        click(){
+            this.addAnimation();
+        }
     }
 });
